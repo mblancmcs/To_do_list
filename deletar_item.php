211f66e99cc77c20ea9619019c263f1114bfcbf7
mblancmcs <mblancm.cs@gmail.com>
2021-02-04 21:38:37 -0300

    3ª Vers. Problema no Insert em tb_concluido

--- deletar_item.php
+++ deletar_item.php
@@ -8,7 +8,8 @@
     $link = $objDb->conecta_mysql();
 
     $deletar_item_conteudo = $_POST['deletar_item'];
-    /*
+    /* -- Quando tentei usar pela SESSION --
+    
     $id_conteudo = $_SESSION['id_conteudo'];
     echo '<pre>';
     print_r($id_conteudo);
@@ -17,11 +18,14 @@
     $sql = "DELETE FROM tb_lista WHERE conteudo = '$deletar_item_conteudo'";
 
     if(mysqli_query($link, $sql)){
-        
+
         $sql = "SELECT id_conteudo FROM tb_lista WHERE conteudo = '$deletar_item_conteudo' ";
+
         if($id_conteudo = mysqli_query($link, $sql)){
 
             $sql = "INSERT INTO tb_concluido(fk_id_conteudo, fk_conteudo) VALUES($id_conteudo, '$deletar_item_conteudo') ";
+
+            header('Location: home.php');
 
         } else {
             echo 'Erro na query';
